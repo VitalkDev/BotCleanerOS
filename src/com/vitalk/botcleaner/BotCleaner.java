0ff331685355c90b3777f10b6f4d715debba5cd9
sickledev <sickledev@gmail.com>
2021-03-01 11:10:59 +0100

    Initial commit

---
+++ src/com/vitalk/botcleaner/BotCleaner.java
@@ -0,0 +1,12 @@
+package com.vitalk.botcleaner;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BotCleaner extends Plugin {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Coming Soon");
+    }
+    //https://github.com/VitalkDev/BotCleaner.git
+}
